bc12b8413cbedeedce9e84899cae645f352d404b
zbipaw <48086190+zbipaw@users.noreply.github.com>
2023-03-28 10:11:30 +0200

    render final scene; finish book

--- src/main.zig
+++ src/main.zig
@@ -16,45 +16,12 @@ const HitRecord = objects.HitRecord;
 const Hittable = objects.Hittable;
 const HittableList = objects.HittableList;
 const Lambertian = material.Lambertian;
+const Material = material.Material;
 const Metal = material.Metal;
 const Point3 = tup3.Point3;
 const Ray = ray.Ray;
 const Sphere = objects.Sphere;
 const Vec3 = tup3.Vec3;
-
-fn four_spheres(allocator: Allocator) !HittableList {
-    var world = HittableList.init(allocator);
-
-    const mat1 = try allocator.create(Lambertian);
-    mat1.* = Lambertian.init(Color.rgb(0.8, 0.8, 0.0));
-    const sphere1 = try allocator.create(Sphere);
-    sphere1.* = Sphere.init(Point3.pos(0, -100.5, -1), 100.0, &mat1.material);
-    try world.add(&sphere1.hittable);
-
-    const mat2 = try allocator.create(Lambertian);
-    mat2.* = Lambertian.init(Color.rgb(0.1, 0.2, 0.5));
-    const sphere2 = try allocator.create(Sphere);
-    sphere2.* = Sphere.init(Point3.pos(0, 0, -1), 0.5, &mat2.material);
-    try world.add(&sphere2.hittable);
-
-    const mat3 = try allocator.create(Dielectric);
-    mat3.* = Dielectric.init(1.5);
-    const sphere3 = try allocator.create(Sphere);
-    sphere3.* = Sphere.init(Point3.pos(-1, 0, -1), 0.5, &mat3.material);
-    try world.add(&sphere3.hittable);
-
-    const sphere5 = try allocator.create(Sphere);
-    sphere5.* = Sphere.init(Point3.pos(-1, 0, -1), -0.4, &mat3.material);
-    try world.add(&sphere5.hittable);
-
-    const mat4 = try allocator.create(Metal);
-    mat4.* = Metal.init(Color.rgb(0.8, 0.6, 0.2), 0.0);
-    const sphere4 = try allocator.create(Sphere);
-    sphere4.* = Sphere.init(Point3.pos(1, 0, -1), 0.5, &mat4.material);
-    try world.add(&sphere4.hittable);
-
-    return world;
-}
 
 fn ray_color(r: Ray, world: *Hittable, depth: u32) Color {
     var hitrec: HitRecord = undefined;
@@ -78,6 +45,79 @@ fn ray_color(r: Ray, world: *Hittable, depth: u32) Color {
     return grad;
 }
 
+fn random_scene(allocator: Allocator) !HittableList {
+    var world = HittableList.init(allocator);
+
+    // Ground
+    var ground_material = try allocator.create(Lambertian);
+    ground_material.* = Lambertian.init(Color.rgb(0.5, 0.5, 0.5));
+    var ground_sphere = try allocator.create(Sphere);
+    ground_sphere.* = Sphere.init(Point3.pos(0, -1000, 0), 1000, &ground_material.material);
+    try world.add(&ground_sphere.hittable);
+
+    // Small spheres
+    var a: i8= -11;
+    while (a < 11) : (a += 1) {
+        var b: i8 = -11;
+        while (b < 11) : (b += 1) {
+            const choose_mat = common.randf();
+            const center = Point3.pos(@intToFloat(f32, a) + 0.9 * common.randf(), 0.2, @intToFloat(f32, b) + 0.9 * common.randf());
+
+            if ((center.sub(Point3.pos(4, 0.2, 0)).len() > 0.9)) {
+                if (choose_mat < 0.8) {
+                    // Lambertian
+                    const albedo = Color.rand().mulv(Color.rand());
+                    var sphere_material = try allocator.create(Lambertian);
+                    sphere_material.* = Lambertian.init(albedo);
+
+                    var sphere = try allocator.create(Sphere);
+                    sphere.* = Sphere.init(center, 0.2, &sphere_material.material);
+                    try world.add(&sphere.hittable);
+                } else if (choose_mat < 0.95) {
+                    // Metal
+                    const albedo = Color.randr(0.5, 1);
+                    const fuzz = common.randrf(0, 0.5);
+                    var sphere_material = try allocator.create(Metal);
+                    sphere_material.* = Metal.init(albedo, fuzz);
+
+                    var sphere = try allocator.create(Sphere);
+                    sphere.* = Sphere.init(center, 0.2, &sphere_material.material);
+                    try world.add(&sphere.hittable);
+                } else {
+                    // Dielectric
+                    var sphere_material= try allocator.create(Dielectric);
+                    sphere_material.* = Dielectric.init(1.5);
+
+                    var sphere = try allocator.create(Sphere);
+                    sphere.* = Sphere.init(center, 0.2, &sphere_material.material);
+                    try world.add(&sphere.hittable);
+                }
+            }
+        }
+    }
+
+    //Big spheres
+    const material1 = try allocator.create(Dielectric);
+    material1.* = Dielectric.init(1.5);
+    const sphere1 = try allocator.create(Sphere);
+    sphere1.* = Sphere.init(Point3.pos(0, 1, 0), 1.0, &material1.material);
+    try world.add(&sphere1.hittable);
+
+    const material2 = try allocator.create(Lambertian);
+    material2.* = Lambertian.init(Color.rgb(0.4, 0.2, 0.1));
+    const sphere2 = try allocator.create(Sphere);
+    sphere2.* = Sphere.init(Point3.pos(-4, 1, 0), 1.0, &material2.material);
+    try world.add(&sphere2.hittable);
+
+    const material3 = try allocator.create(Metal);
+    material3.* = Metal.init(Color.rgb(0.7, 0.6, 0.5), 0.0);
+    const sphere3 = try allocator.create(Sphere);
+    sphere3.* = Sphere.init(Point3.pos(4, 1, 0), 1.0, &material3.material);
+    try world.add(&sphere3.hittable);
+
+    return world;
+}
+
 pub fn main() !void {
     //allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -95,11 +135,11 @@ pub fn main() !void {
     const aspect_ratio: f32 = 3.0 / 2.0;
     const image_width: u32 = 480;
     const image_height: u32 = @floatToInt(u32, @as(f32, image_width) / aspect_ratio);
-    const max_depth: u32 = 64; 
-    const samples_per_pixel: u32 = 256;
+    const max_depth: u32 = 16; 
+    const samples_per_pixel: u32 = 64;
 
     //World
-    var world = try four_spheres(allocator);
+    var world = try random_scene(allocator);
 
     //Camera
     const cam_lookfrom = Point3.pos(13.0, 2.0, 3.0);
